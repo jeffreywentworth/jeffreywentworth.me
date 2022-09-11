a6a11c6ca82362ad28dfeb924230eb457cd00229
Jefffrey Wentworth <jeffreyjwentworth@gmail.com>
2022-09-11 16:39:04 -0400

    typo fix

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,21 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
-}
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		trailingSlash: 'always',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html',
+			precompress: false
+		})
+	}
+};
 
 export default config;
